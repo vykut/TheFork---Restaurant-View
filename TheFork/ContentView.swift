3b487143f9bc255d7b219eab35e3b2fb4104ada0
Victor Socaciu <socaciu.victor@gmail.com>
2020-11-27 17:51:07 +0200

    Initial Commit

---
+++ TheFork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheFork
+//
+//  Created by Victor Socaciu on 27.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheFork/TheForkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheForkApp.swift
+//  TheFork
+//
+//  Created by Victor Socaciu on 27.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheForkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
